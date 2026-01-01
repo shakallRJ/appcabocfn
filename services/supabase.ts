c9d72fd2852b583b4b954e580f3e701834781fbc
shakallRJ <jairoedryka@gmail.com>
2026-01-01 16:22:05 -0300

    Update supabase.ts

--- services/supabase.ts
+++ services/supabase.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // No Vercel, você configurará estas variáveis de ambiente
-const supabaseUrl = process.env.SUPABASE_URL || 'https://sua-url-aqui.supabase.co';
+const supabaseUrl = process.env.SUPABASE_URL ||'postgresql://postgres:123marinha123@db.ocoobqyxxhcpbuqoreay.supabase.co:5432/postgres';
 const supabaseKey = process.env.SUPABASE_ANON_KEY || 'sua-chave-anon-aqui';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
